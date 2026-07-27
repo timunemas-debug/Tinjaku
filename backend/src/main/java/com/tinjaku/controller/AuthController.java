867976094d97b4fb5002d18ab832e73001b4148f
Jeremy <jeremyputradarma1@gmail.com>
2026-07-28 00:24:24 +0700

    feat: add authentication controller

--- backend/src/main/java/com/tinjaku/controller/AuthController.java
+++ backend/src/main/java/com/tinjaku/controller/AuthController.java
@@ -1,10 +1,35 @@
 package com.tinjaku.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tinjaku.dto.request.LoginRequest;
+import com.tinjaku.dto.request.RegisterRequest;
+import com.tinjaku.dto.response.LoginResponse;
+import com.tinjaku.dto.response.RegisterResponse;
+import com.tinjaku.service.AuthService;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
     
+    private final AuthService authService;
+
+    public AuthController(AuthService authService){
+        this.authService = authService;
+    }
+
+    @PostMapping("/regist")
+    public RegisterResponse regist(@RequestBody @Valid RegisterRequest request){
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public LoginResponse login(@RequestBody @Valid LoginRequest request){
+        return authService.login(request);
+    }
 }
