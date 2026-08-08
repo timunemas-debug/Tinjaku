6ff87158078b0d3bdcdf4a5848d1463f19e006d9
Jeremy <jeremyputradarma1@gmail.com>
2026-08-08 11:31:08 +0700

    feat: update NotificationService

--- backend/src/main/java/com/tinjaku/service/NotificationService.java
+++ backend/src/main/java/com/tinjaku/service/NotificationService.java
@@ -1,8 +1,14 @@
 package com.tinjaku.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.tinjaku.dto.response.NotificationResponse;
 import com.tinjaku.mapper.NotificationMapper;
+import com.tinjaku.model.Notification;
+import com.tinjaku.model.User;
 import com.tinjaku.repository.NotificationRepository;
 
 @Service
@@ -10,9 +16,46 @@ public class NotificationService {
     
     private final NotificationRepository notificationRepository;
     private final NotificationMapper notificationMapper;
+    private final UserService userService;
 
-    public NotificationService(NotificationRepository notificationRepository, NotificationMapper notificationMapper){
+    public NotificationService(NotificationRepository notificationRepository, NotificationMapper notificationMapper, UserService userService){
         this.notificationRepository = notificationRepository;
         this.notificationMapper = notificationMapper;
+        this.userService = userService;
+    }
+
+    public NotificationResponse sendNotification(Long userId, String message){
+
+        User user = userService.getUserById(userId);
+
+        Notification notification = new Notification();
+        notification.setMessage(message);
+        notification.setCreatedAt(LocalDateTime.now());
+        notification.setRead(false);
+        notification.setUser(user);
+
+        return notificationMapper.toResponse(notificationRepository.save(notification));
+    }
+
+    public List<NotificationResponse> getMyNotifications(Long userId){
+        return notificationRepository
+                .findByUserUserId(userId)
+                .stream()
+                .map(notificationMapper::toResponse)
+                .toList();
+    }
+
+    public List<NotificationResponse> getUnreadNotifications(Long userId){
+
+        return notificationRepository
+                .findByUserUserIdIsRead(userId, false)
+                .stream()
+                .map(notificationMapper::toResponse)
+                .toList();
+    }
+
+    public boolean hasUnreadNotifications(Long userId){
+        
+        return notificationRepository.existsByUserUserIdIsRead(userId, false);
     }
 }
